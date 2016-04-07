f320fe05a54261551473c03827a2ec5c521afbe1
FredrikRissanen <frissanen@hotmail.com>
2016-04-07 12:54:26 +0200

    Manual merge/override

--- Controller/main.py
+++ Controller/main.py
@@ -1,87 +1,96 @@
+from logging import root
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
 from tkinter import filedialog
+import tkinter as tk
+from tkinter.ttk import Style
 
-root = Tk()
-
-w = 800
-h = 650
-
-ws = root.winfo_screenwidth()
-hs = root.winfo_screenheight()
-
-x = (ws/2) - (w/2)
-y = (hs/2) - (h/2)
-
-root.geometry('%dx%d+%d+%d' % (w, h, x, y))
-
-root.wm_title('MARSEM - Miraculously Autonomous Rover Search Environment Mapper')
-
-root.lift()
-root.attributes('-topmost', True)
-root.after_idle(root.attributes, '-topmost', False)
-
-panedWindow = ttk.PanedWindow(root, orient = HORIZONTAL)
-panedWindow.pack(fill = BOTH, expand = True)
-
-framepictureset = ttk.Frame(panedWindow, width = 100, height = 300, relief = SUNKEN)
-framemain = ttk.Frame(panedWindow, width = 300, height = 300, relief = SUNKEN)
-
-panedWindow.add(framepictureset, weight = 0)
-panedWindow.add(framemain, weight = 1)
 
 # Set which directory to save downloaded picture sets in.
-def setdirectory():
-    print 'Set up where picture sets will be saved'
+def setDirectory():
+    print('Set up where picture sets will be saved')
+    filedialog.askdirectory()
 
 
 # Send go ahead signal to Pi to start picture taking sequence.
-def startpicturetaking():
-    print 'Send shit to Pi'
+def startPictureTaking():
+    print('Send shit to Pi')
+
+
+def aboutAuthors():
+    print('Stuff about author')
+    infoWindow = Toplevel(root)
+    infoWindow.title('About Authors')
+    infoWindow.lift(root)
+
+    info = Text(infoWindow, width=40, height=10).pack()
 
 
 # Popup window when Pi sends a signal that picture sequence has ended.
-def askwhenfinished():
-    if messagebox.askyesno(title = 'Finished', message = 'Would you like to download the picture?') == True:
-        print 'You pressed yes'
+def askWhenFinished():
+    if messagebox.askyesno(title='Finished', message='Would you like to download the picture?') == True:
+        print('You pressed yes')
     else:
-        print 'You pressed no'
+        print('You pressed no')
 
 
 # Function to download taken pictures.
-def downloadpictureset():
-    print 'Download currently selected picture set'
+def downloadPictureSet():
+    print('Download currently selected picture set')
 
 
 # Ability to browse picture sets on the Pi to download them.
-def browsepicturesetsonpi():
-    print 'Show all folders with pictures on PI, to be able to download older content that has not been downloaded'
+def browsePictureSetsOnPi():
+    print('Show all folders with pictures on PI, to be able to download older content that has not been downloaded')
 
 
-root.option_add('*tearOff', False)
-menubar = Menu(root)
-root.config(menu = menubar)
+class Window:
+    def __init__(self, master):
+        master.configure(background='Blue')
 
-settings = Menu(menubar)
-menubar.add_cascade(menu = settings, label = 'Settings')
-settings.add_command(label = 'Directory to save in', command = setdirectory)
+        self.frame_library = tk.Frame(master)
+        self.frame_library.pack(side=LEFT)
 
-help_ = Menu(menubar)
-menubar.add_cascade(menu = help_, label = 'Help')
-help_.add_command(label = 'Stuff')
+        self.frame_main = tk.Frame(master)
+        self.frame_main.config(bg='Green')
+        self.frame_main.pack()
 
-about = Menu(menubar)
-menubar.add_cascade(menu = about, label = 'About')
-about.add_command(label = 'Developers')
+        tk.Button(self.frame_main, text='Start').grid(row = 5, column = 1)
+        progress = ttk.Progressbar(self.frame_main).grid(row = 6, column =2 )
+        tk.Button(self.frame_main, text='Download').grid(row =7 , column = 3)
+        download = ttk.Progressbar(self.frame_main).grid(row =15 , column = 2)
+        tk.Button(self.frame_library, text='test').grid(row =9 , column = 3)
+
+        self.menu = Menu(master)
+        master.config(menu=self.menu)
+
+        self.settings = Menu(self.menu)
+        self.about = Menu(self.menu)
+        self.help_ = Menu(self.menu)
+
+        self.menu.add_cascade(menu=self.settings, label='Settings')
+        self.settings.add_command(label='Choose Directory', command = lambda: setDirectory())
+
+        self.menu.add_cascade(menu=self.about, label='Authors')
+        self.about.add_command(label='About authors', command = lambda: aboutAuthors())
+
+        self.menu.add_cascade(menu=self.help_, label='Help')
+        self.help_.add_command(label='Stuff')
 
 
-btnstart = ttk.Button(framemain, text = 'Start', command = startpicturetaking).pack()
-progresspictures = ttk.Progressbar(framemain).pack()
+def main():
+    root = Tk()
+    root.lift()
+    root.attributes('-topmost', True)
+    root.after_idle(root.attributes, '-topmost', False)
+    root.wm_title('MARSEM - Miraculously Autonomous Rover Search Environment Mapper')
+    root.resizable(FALSE, FALSE)
+    root.geometry('640x480+50+100')
+    root.option_add('*tearOff', False)
+    window = Window(root)
+
+    root.mainloop()
 
 
-btndownload = ttk.Button(framemain, text = 'Download', command = downloadpictureset).pack()
-progressdownload = ttk.Progressbar(framemain).pack()
-
-
-root.mainloop()
+if __name__ == "__main__": main()
