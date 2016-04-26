e38c9aa6cd143f16f39d6fb6dacd1268ba23cd0e
FredrikRissanen <frissanen@hotmail.com>
2016-04-26 17:50:55 +0200

    Removed item factory, added menu manually

--- Marsem/Interface/window.py
+++ Marsem/Interface/window.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
-import pygtk
-
-pygtk.require('2.0')
 import gtk
+import pygtk
+pygtk.require('2.0')
 
-def picture_clicked(self):
-    print"take picture"
+def open_file(self):
+    print "Open"
+
+
+def save_file(self):
+    print "Save"
+
+def quit_file(self):
+    print "Quit"
 
 def start_clicked(self):
     print "Start"
@@ -15,62 +21,42 @@ def download_clicked(self):
     print "Download"
 
 
-def new_file(self):
-    print "Creates a file"
-
-
-def open_file(self):
-    print "Opens a file"
-
-
-def save_file(self):
-    print "Saves a file"
-
-
-def save_as_file(self):
-    print "Saves a file as"
-
-
-def options_menu(self):
-    print "options"
-
-
-def help_menu(self):
-    print "help"
+def picture_clicked(self):
+    print "Image taken"
 
 
 class Menu:
-    def print_hello(self, w, data):
-        print "Welcome to Marsem"
-
-    def get_main_menu(self, window):
-        accel_group = gtk.AccelGroup()
-        item_factory = gtk.ItemFactory(gtk.MenuBar, "<main>", accel_group)
-        item_factory.create_items(self.menu_items)
-        window.add_accel_group(accel_group)
-        self.item_factory = item_factory
-        return item_factory.get_widget("<main>")
-
-
     def __init__(self):
-        self.menu_items = (
-            ("/_File", None, None, 0, "<Branch>"),
-            ("/File/_New", "<control>N", new_file(self), 0, None),
-            ("/File/_Open", "<control>O", open_file(self), 0, None),
-            ("/File/_Save", "<control>S", save_file(self), 0, None),
-            ("/File/Save _As", None, save_as_file(self), 0, None),
-            ("/File/sep1", None, None, 0, "<Separator>"),
-            ("/File/Quit", "<control>Q", gtk.main_quit, 0, None),
-            ("/_Options", None, options_menu(self), 0, "<Branch>"),
-            ("/Options/Test", None, None, 0, None),
-            ("/_Help", None, help_menu(self), 0, "<LastBranch>"),
-            ("/_Help/About", None, None, 0, None),
-        )
         window = gtk.Window(gtk.WINDOW_TOPLEVEL)
         window.connect("destroy", lambda wid: gtk.main_quit())
         window.connect("delete_event", lambda a1, a2: gtk.main_quit())
         window.set_title("Marsem")
         window.set_size_request(300, 200)
+
+        file_menu = gtk.Menu()  # Don't need to show menus
+        #Menu Items
+        open_item = gtk.MenuItem("Open")
+        save_item = gtk.MenuItem("Save")
+        quit_item = gtk.MenuItem("Quit")
+
+        file_menu.append(open_item)
+        file_menu.append(save_item)
+        file_menu.append(quit_item)
+
+        #CallBack to menu items
+        open_item.connect_object("activate", open_file, "file.open")
+        save_item.connect_object("activate", save_file, "file.save")
+        quit_item.connect_object("activate", quit_file, "file.quit")
+
+        open_item.show()
+        save_item.show()
+        quit_item.show()
+
+        menu_bar = gtk.MenuBar()
+        file_item = gtk.MenuItem("File")
+        file_item.show()
+        file_item.set_submenu(file_menu)
+        menu_bar.append(file_item)
 
         main_box = gtk.VBox(False, 1)
         main_box.set_border_width(1)
@@ -79,10 +65,10 @@ class Menu:
 
         button_box = gtk.HBox(False, 20)
         button_box.set_border_width(0)
-        window.add(button_box)
         button_box.show()
-        menubar = self.get_main_menu(window)
-
+        start_clicked
+        download_clicked
+        picture_clicked
         # Buttons
         # Start
         start_button = gtk.Button(label="Start", stock=None)
@@ -100,14 +86,14 @@ class Menu:
         picture_button.set_size_request(width=90, height=20)
         picture_button.show()
 
-        main_box.pack_start(menubar, False, True, 0)
+        main_box.pack_start(menu_bar, False, True, 0)
         main_box.pack_end(button_box, False, True, 0)
         button_box.pack_start(start_button, False, False, 10)
         button_box.pack_start(download_button, False, False, 10)
         button_box.pack_end(picture_button, False, False, 10)
 
         button_box.show
-        menubar.show()
+        menu_bar.show()
         window.show()
 
 
