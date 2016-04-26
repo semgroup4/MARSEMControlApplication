47ee4c2b72fb28ca4952ca0b077e02a506483307
Philip Crockett <philipmayoo@gmail.com>
2016-04-26 11:09:01 +0200

    The connection can now tell the car where to move next

--- src/connect.py
+++ src/connect.py
@@ -1,5 +1,35 @@
 #!/usr/bin/env
+# author: phcr
+# The API to tell the car how to operate.
+import httplib, urllib
+
+# Server IP & Port
+# CAR IP: 192.168.2.1
+config = {
+    "host": "localhost",
+    "port": "8000",
+    "headers": {"Content-Type": "application/json"},
+}
+
+# Connect to the server, creates a HTTPConnection type
+connection = httplib.HTTPConnection(config['host'], config['port'])
+
+connection.connect()
+# desc: sends a move action to the Car
+# returns: HTTPResponse Object
+# docs: https://docs.python.org/2/library/httplib.html#httpresponse-objects
+def move(action=None):
+    connection.request("GET", "/", params(action), config['headers'])
+    return connection.getresponse()
 
 
-def connect():
-    
+# desc: gets the current status of the Car @NotImplemented
+# returns: HTTPResponse Object
+# docs: https://docs.python.org/2/library/httplib.html#httpresponse-objects
+def status():
+    connection.request("GET", "/status")
+    return connection.getresponse()
+
+
+def params(param):
+     return urllib.urlencode({"action": param})
