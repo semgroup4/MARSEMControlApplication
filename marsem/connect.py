297ef064b26ce286ded4711c1f43936cbf04e012
Philip Crockett <philipmayoo@gmail.com>
2016-04-28 12:12:27 +0200

    Changed to requests

--- marsem/connect.py
+++ marsem/connect.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env
-# author: phcr
-# The API to tell the car how to operate.
-import httplib, urllib
+
+import requests
 
 # Server IP & Port
 # CAR IP: 192.168.2.1
@@ -11,25 +10,9 @@ config = {
     "headers": {"Content-Type": "application/json"},
 }
 
-# Connect to the server, creates a HTTPConnection type
-connection = httplib.HTTPConnection(config['host'], config['port'])
+host = "http://" + config['host'] + ":" + config['port']
 
-connection.connect()
 # desc: sends a move action to the Car
-# returns: HTTPResponse Object
-# docs: https://docs.python.org/2/library/httplib.html#httpresponse-objects
 def move(action=None):
-    connection.request("GET", "/", params(action), config['headers'])
-    return connection.getresponse()
-
-
-# desc: gets the current status of the Car @NotImplemented
-# returns: HTTPResponse Object
-# docs: https://docs.python.org/2/library/httplib.html#httpresponse-objects
-def status():
-    connection.request("GET", "/status")
-    return connection.getresponse()
-
-
-def params(param):
-     return urllib.urlencode({"action": param})
+    r = requests.get(host, params={"action": action}, headers=config['headers'])
+    return r.text;
