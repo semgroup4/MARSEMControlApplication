f9aec9c49e082dedd7f2cc1746bba1d76892d523
Måns Thörnvik <thornvik.mans@gmail.com>
2016-04-30 18:42:47 +0200

    Done lots of things here: Created a basic layout in .kv file to set the look and feel of all views or "screens" at once. Added two screens, home and settings. The first is for controlling and starting everything, the second for configuring (logic.). So far the two screens can be switched between and nothing more than that.

---
+++ Marsem/pyKivy/kivyApp.py
@@ -0,0 +1,46 @@
+from kivy.app import App
+
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.progressbar import ProgressBar
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+
+
+class HomeScreen(Screen):
+    pass
+
+
+class SettingsScreen(Screen):
+    pass
+
+
+class CustomLayout(Widget):
+    pass
+
+
+class Decorations(Widget):
+    pass
+
+
+class StartButton(Button):
+
+    def start(self, *args):
+        print 'Start-the-car-code goes here'
+
+
+class PhotoProgress(ProgressBar):
+    pass
+
+
+class Marsem(App):
+
+    def build(self):
+        screenmanager = ScreenManager(transition=FadeTransition())
+        screenmanager.add_widget(HomeScreen(name='home'))
+        screenmanager.add_widget(SettingsScreen(name='settings'))
+
+        return screenmanager
+
+
+if __name__ == "__main__":
+    Marsem().run()
