cd6d6f66615e7d40f0e38cad922e01e4d63e927d
FredrikRissanen <frissanen@hotmail.com>
2016-05-01 21:16:19 +0200

    Added a menu with file manager and settings

---
+++ Marsem/pyKivy/kivyApp.py
@@ -0,0 +1,92 @@
+import os
+
+from kivy.app import App
+from kivy.factory import Factory
+from kivy.properties import ObjectProperty
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.popup import Popup
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.progressbar import ProgressBar
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+
+
+class HomeScreen(Screen):
+    def dismiss_popup(self):
+        self._popup.dismiss()
+
+    # Shows open file
+    def show_load(self):
+        content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Load file", content=content,
+                            size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    # Shows save file
+    def show_save(self):
+        content = SaveDialog(save=self.save, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Save file", content=content,
+                            size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    def load(self, path, filename):
+        with open(os.path.join(path, filename[0])) as stream:
+            self.text_input.text = stream.read()
+
+        self.dismiss_popup()
+
+    def save(self, path, filename):
+        with open(os.path.join(path, filename), 'w') as stream:
+            stream.write(self.text_input.text)
+
+        self.dismiss_popup()
+
+
+class CustomLayout(Widget):
+    pass
+
+
+class Decorations(Widget):
+    pass
+
+
+class Menu(FloatLayout):
+    pass
+
+
+class StartButton(Button):
+    def start(self, *args):
+        print 'Start-the-car-code goes here'
+
+
+class PhotoProgress(ProgressBar):
+    pass
+
+
+class LoadDialog(FloatLayout):
+    load = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
+
+class SaveDialog(FloatLayout):
+    save = ObjectProperty(None)
+    text_input = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
+
+class Root(FloatLayout):
+    loadfile = ObjectProperty(None)
+    savefile = ObjectProperty(None)
+    text_input = ObjectProperty(None)
+
+
+class Marsem(App):
+    def build(self):
+        Factory.register('Root', cls=Root)
+        Factory.register('LoadDialog', cls=LoadDialog)
+        Factory.register('SaveDialog', cls=SaveDialog)
+        return HomeScreen()
+
+
+if __name__ == "__main__":
+    Marsem().run()
