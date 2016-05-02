6d11da165c5bac60093a2a34b7ae5ace8a4099b6
FredrikRissanen <frissanen@hotmail.com>
2016-05-02 11:31:49 +0200

    Added video stream to main view

--- Marsem/UserKivyFace/kivyApp.py
+++ Marsem/UserKivyFace/kivyApp.py
@@ -1,6 +1,7 @@
 import os
 
 from kivy.app import App
+from kivy.atlas import CoreImage
 from kivy.factory import Factory
 from kivy.properties import ObjectProperty
 from kivy.uix.floatlayout import FloatLayout
@@ -9,7 +10,61 @@ from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.progressbar import ProgressBar
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+import io
+import urllib
+import threading
+from kivy.uix.image import Image
+from kivy.app import App
+from kivy.properties import StringProperty
+from kivy.core.image import Image as CoreImage
+from kivy.clock import Clock
+from collections import deque
 
+class MjpegViewer(Image):
+
+    url = StringProperty()
+
+    def start(self):
+        self.quit = False
+        self._queue = deque()
+        self._thread = threading.Thread(target=self.read_stream)
+        self._thread.daemon = True
+        self._thread.start()
+        self._image_lock = threading.Lock()
+        self._image_buffer = None
+        Clock.schedule_interval(self.update_image, 1 / 30.)
+
+    def stop(self):
+        self.quit = True
+        self._thread.join()
+        Clock.unschedule(self.read_queue)
+
+    def read_stream(self):
+        stream = urllib.urlopen(self.url)
+        bytes = ''
+        while not self.quit:
+            bytes += stream.read(1024)
+            a = bytes.find('\xff\xd8')
+            b = bytes.find('\xff\xd9')
+            if a != -1 and b != -1:
+                jpg = bytes[a:b + 2]
+                bytes = bytes[b + 2:]
+
+                data = io.BytesIO(jpg)
+                im = CoreImage(data,
+                               ext="jpeg",
+                               nocache=True)
+                with self._image_lock:
+                    self._image_buffer = im
+
+    def update_image(self, *args):
+        im = None
+        with self._image_lock:
+            im = self._image_buffer
+            self._image_buffer = None
+        if im is not None:
+            self.texture = im.texture
+            self.texture_size = im.texture.size
 
 class HomeScreen(Screen):
     def dismiss_popup(self):
@@ -40,7 +95,6 @@ class HomeScreen(Screen):
             stream.write(self.text_input.text)
 
         self.dismiss_popup()
-
 
 class CustomLayout(Widget):
     pass
@@ -82,11 +136,23 @@ class Root(FloatLayout):
 
 class Marsem(App):
     def build(self):
+        main_box = FloatLayout()
+
         Factory.register('Root', cls=Root)
         Factory.register('LoadDialog', cls=LoadDialog)
         Factory.register('SaveDialog', cls=SaveDialog)
-        return HomeScreen()
+        home = HomeScreen()
+
+        viewer = MjpegViewer(
+        url="http://195.235.198.107:3344/axis-cgi/mjpg/video.cgi?resolution=320x240")
+        viewer.start()
+
+        main_box.add_widget(home)
+        main_box.add_widget(viewer)
+
+        return main_box
 
 
 if __name__ == "__main__":
+
     Marsem().run()
