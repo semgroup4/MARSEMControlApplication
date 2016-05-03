21bc84d8cbfb1d08923104e4c6afe4f94abfb74e
Philip Crockett <philipmayoo@gmail.com>
2016-05-03 09:01:39 +0200

    Real host name

--- marsem/protocol/config.py
+++ marsem/protocol/config.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 
 config = {
-    "host": "localhost",
+    "host": "192.168.2.1",
     "port": "8000",
     "headers": {"Content-Type": "application/json"},
     "stream-port": "2222",
-    "remote-user": "pi",
-    "remote-p": "raspberry",
 }
 
 host_index = "http://" + config['host'] + ":" + config['port']
