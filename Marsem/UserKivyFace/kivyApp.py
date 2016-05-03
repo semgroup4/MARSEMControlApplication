089382cdb1661e4067d17ff0f99a6965557775e3
FredrikRissanen <frissanen@hotmail.com>
2016-05-03 12:33:35 +0200

    Added objecproperty

--- Marsem/UserKivyFace/kivyApp.py
+++ Marsem/UserKivyFace/kivyApp.py
@@ -5,6 +5,7 @@ import threading
 from kivy.app import App
 from kivy.atlas import CoreImage
 from kivy.factory import Factory
+from kivy.properties import ObjectProperty
 from kivy.uix.dropdown import DropDown
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.popup import Popup
@@ -146,15 +147,20 @@ class PhotoProgress(ProgressBar):
 
 
 class LoadDialog(FloatLayout):
-    pass
+    load = ObjectProperty(None)
+    cancel = ObjectProperty(None)
 
 
 class SaveDialog(FloatLayout):
-    pass
+    save = ObjectProperty(None)
+    text_input = ObjectProperty(None)
+    cancel = ObjectProperty(None)
 
 
 class Root(FloatLayout):
-    pass
+    loadfile = ObjectProperty(None)
+    savefile = ObjectProperty(None)
+    text_input = ObjectProperty(None)
 
 
 class Marsem(App):
