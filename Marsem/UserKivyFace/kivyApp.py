ed413a8dd2fe59ae53ac71b20d4e029352609155
FredrikRissanen <frissanen@hotmail.com>
2016-05-03 12:44:11 +0200

    Removed unused files

--- Marsem/UserKivyFace/kivyApp.py
+++ Marsem/UserKivyFace/kivyApp.py
@@ -1,22 +1,19 @@
 import os
-import io
-import urllib
-import threading
+
 from kivy.app import App
-from kivy.atlas import CoreImage
+
 from kivy.factory import Factory
 from kivy.properties import ObjectProperty
+from kivy.uix import video
 from kivy.uix.dropdown import DropDown
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.popup import Popup
+from kivy.uix.video import Video
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.progressbar import ProgressBar
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
-from kivy.uix.image import Image
-from kivy.core.image import Image as CoreImage
-from kivy.clock import Clock
-from collections import deque
+
 
 
 class HomeScreen(Screen):
@@ -50,8 +47,17 @@ class HomeScreen(Screen):
         self.dismiss_popup()
 
 
-class VideoStream(Image):
-    pass
+class VideoStream(Video):
+    def on_position_change(instance, value):
+        print('The position in the video is', value)
+
+    def on_duration_change(instance, value):
+        print('The duration of the video is', video)
+
+    video = Video(source='PandaSneezes.avi')
+    video.bind(position=on_position_change,
+               duration=on_duration_change)
+
 
 class SettingsScreen(Screen):
     pass
@@ -77,64 +83,11 @@ class Menu(FloatLayout):
     pass
 
 
-class DamnButton(Button):
+class DropDown(Button):
     def __init__(self, **kw):
-        super(DamnButton, self).__init__(**kw)
+        super(DropDown, self).__init__(**kw)
         self.ddn = DropDown()
         self.ddn.bind(on_release=self.on_release)
-
-
-class DamnButton2(Button):
-    pass
-
-
-"""
-class MjpegViewer(Image):
-    url = "http://195.235.198.107:3344/axis-cgi/mjpg/video.cgi?resolution=320x240"
-
-
-    def start(self):
-        self.quit = False
-        self._queue = deque()
-        self._thread = threading.Thread(target=self.read_stream)
-        self._thread.daemon = True
-        self._thread.start()
-        self._image_lock = threading.Lock()
-        self._image_buffer = None
-        Clock.schedule_interval(self.update_image, 1 / 30.)
-
-    def stop(self):
-        self.quit = True
-        self._thread.join()
-        Clock.unschedule(self.read_queue)
-
-    def read_stream(self):
-        stream = urllib.urlopen(self.url)
-        bytes = ''
-        while not self.quit:
-            bytes += stream.read(1024)
-            a = bytes.find('\xff\xd8')
-            b = bytes.find('\xff\xd9')
-            if a != -1 and b != -1:
-                jpg = bytes[a:b + 2]
-                bytes = bytes[b + 2:]
-
-                data = io.BytesIO(jpg)
-                im = CoreImage(data,
-                               ext="jpeg",
-                               nocache=True)
-                with self._image_lock:
-                    self._image_buffer = im
-
-    def update_image(self, *args):
-        im = None
-        with self._image_lock:
-            im = self._image_buffer
-            self._image_buffer = None
-        if im is not None:
-            self.texture = im.texture
-            self.texture_size = im.texture.size
-"""
 
 
 class StartButton(Button):
@@ -169,15 +122,10 @@ class Marsem(App):
         Factory.register('LoadDialog', cls=LoadDialog)
         Factory.register('SaveDialog', cls=SaveDialog)
 
-        #        viewer = MjpegViewer()
-        #        viewer.start()
-
         screenManager = ScreenManagement()
         screenManager.add_widget(HomeScreen())
         screenManager.add_widget(SettingsScreen())
         screenManager.add_widget(PhotoScreen())
-
-        #        main.add_widget(viewer)
 
         return screenManager
 
