02902652e54856312d8f56c04a05b92bcafdf622
FredrikRissanen <frissanen@hotmail.com>
2016-05-11 13:05:43 +0200

    Moved menu to buttons made function to load images

--- marsem/gui/kivyApp.py
+++ marsem/gui/kivyApp.py
@@ -1,16 +1,12 @@
 import os
+from PIL import Image
 from kivy.app import App
-from kivy.factory import Factory
-from kivy.properties import ObjectProperty, StringProperty
-from kivy.uix.dropdown import DropDown
-from kivy.uix.filechooser import FileChooserController, FileChooserIconView
 from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.popup import Popup
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.progressbar import ProgressBar
-from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 
 class HomeScreen(Screen):
@@ -22,10 +18,37 @@ class SettingsScreen(Screen):
 
 
 class PhotoScreen(Screen):
-    pass
+    # provides filechooser path
+    def loadImage(self, fileChooser):
+        global imagePath
+        imagePath = fileChooser.path
+
+        return print(fileChooser.path)
+
+
+class PhotoViewer(Screen):
+    #Load images from filechooser path
+    def showImages(self):
+        try:
+            def listFiles(path):
+                files = []
+                for base, directory, filename in os.walk(path):
+                    for i in range(len(filename)):
+                        str = filename[i]
+                        if str.find('.png') > 0:                            #Breakout PNG
+                            img = Image.open(base + "/" + filename[i])
+                            img.show()
+                            files.append(base + "/" + filename[i])
+                return print(files)
+
+            listFiles(imagePath)
+        except NameError:
+            pass
+
 
 class PhotoController(StackLayout):
     pass
+
 
 class ScreenManagement(ScreenManager):
     pass
@@ -40,42 +63,7 @@ class Decorations(Widget):
 
 
 class Menu(FloatLayout):
-
-    def dismiss_popup(self):
-        self._popup.dismiss()
-
-    # Shows open file
-    def show_load(self):
-
-        content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
-        self._popup = Popup(title="Load file", content=content,
-                            size_hint=(0.9, 0.9))
-        self._popup.open()
-
-    # Shows save file
-    def show_save(self):
-        content = SaveDialog(save=self.save, cancel=self.dismiss_popup)
-        self._popup = Popup(title="Save file", content=content,
-                            size_hint=(0.9, 0.9))
-        self._popup.open()
-
-    def load(self, path, filename):
-        with open(os.path.join(path, filename[0])) as stream:
-                self.text_input.text = stream.read()
-                self.dismiss_popup()
-
-    def save(self, path, filename):
-        with open(os.path.join(path, filename), 'w') as stream:
-            stream.write(self.text_input.text)
-
-        self.dismiss_popup()
-
-
-class DropDown(Button):
-    def __init__(self, **kw):
-        super(DropDown, self).__init__(**kw)
-        self.ddn = DropDown()
-        self.ddn.bind(on_release=self.on_release)
+    pass
 
 
 class StartButton(Button):
@@ -87,36 +75,9 @@ class PhotoProgress(ProgressBar):
     pass
 
 
-class LoadDialog(FloatLayout):
-    load = ObjectProperty(None)
-    cancel = ObjectProperty(None)
-
-
-class SaveDialog(FloatLayout):
-    save = ObjectProperty(None)
-    text_input = ObjectProperty(None)
-    cancel = ObjectProperty(None)
-
-
-class Root(FloatLayout):
-    rootpath = "Users/Frank/Documents/kivy"
-    loadfile = ObjectProperty(None)
-    savefile = ObjectProperty(None)
-    text_input = ObjectProperty(None)
-
-
 class Marsem(App):
     def build(self):
-        Factory.register('Root', cls=Root)
-        Factory.register('LoadDialog', cls=LoadDialog)
-        Factory.register('SaveDialog', cls=SaveDialog)
-
-        screenManager = ScreenManagement()
-        screenManager.add_widget(HomeScreen())
-        screenManager.add_widget(SettingsScreen())
-        screenManager.add_widget(PhotoScreen())
-
-        return screenManager
+        return ScreenManagement()
 
 
 if __name__ == "__main__":
