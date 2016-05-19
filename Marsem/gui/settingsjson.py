bccc8795e3795b5e53464b1a67dcfdd5c67348c4
Philip Crockett <philipmayoo@gmail.com>
2016-05-19 14:59:42 +0200

    Removed Marsem

--- Marsem/gui/settingsjson.py
+++
@@ -1,9 +0,0 @@
-
-import json
-
-settings_json = json.dumps([
-    {'type': 'path',
-     'title': 'Save Path',
-     'desc': 'Path',
-     'section': 'section_settings',
-     'key': 'save_path'}])
