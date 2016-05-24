b8d1acf8648b6b1742b1b1f4fab0dfb495ba44d4
IArason <isar.arason@outlook.com>
2016-05-24 19:33:07 -0400

    Looks finalized save for a bug with loading the snapshot image. See lengthy comment in calibrationScreen.py.

--- marsem/gui/calibrationScreen.py
+++ marsem/gui/calibrationScreen.py
@@ -4,12 +4,11 @@
 import marsem.protocol.car as car
 from marsem.opencv import Color
 from kivy.uix.screenmanager import Screen
-from kivy.uix.image import Image as KivyImage
-from kivy.uix.behaviors import ButtonBehavior
 from kivy.lang import Builder
-from kivy.graphics import Rectangle, Point, GraphicException
+from kivy.properties import StringProperty
 
 from PIL import Image
+Image.LOAD_TRUNCATED_IMAGES = True
 
 import numpy
 
@@ -20,6 +19,9 @@ class CalibrationScreen(Screen):
 
     color = Color()
 
+    color_string = StringProperty()
+    current_action_string = StringProperty()
+
     # NOTE:
     # The pixel selector is extremely hacky and relies on hardcoded
     # image position values. These values must be adjusted if the UI
@@ -27,6 +29,9 @@ class CalibrationScreen(Screen):
 
     image_dimensions = 420, 340
     image_offset = 50, 50
+
+    # -1 none, 0 min, 1 max
+    selection = -1
 
     # Grabs an image from the camera and saves to file
     def update_picture(self):
@@ -38,42 +43,74 @@ class CalibrationScreen(Screen):
 
         file = open('calibImage.jpg','wb+')
         file.write(new_pic)
+        file.close()
 
-    # Returns a pretty string of the color range
-    def get_color_string(self):
-        return 'Min: ' + self.color.min.__str__() + \
+    # Updates the label with the color values
+    def update_color_string(self):
+        self.color_string = 'Min: ' + self.color.min.__str__() + \
                '\nMax: ' + self.color.max.__str__()
-
-    # Returns the pixel at x, y on the calibImage
-    def get_pixel_at(self, x, y):
-        im = Image.open('calibImage.jpg')
-        if(x >= im.width or y >= im.height or x < 0 or y < 0):
-            print("Invalid pixel coordinate")
-            return
-
-        return im.getpixel((x, y))
 
     # Returns the selected color range
     def get_color(self):
         return self.color
 
+    # Makes the next click select the min color
+    def start_select_min(self):
+        self.current_action_string = 'Selecting Min'
+        self.selection = 0
+
+    # Makes the next click select the max color
+    def start_select_max(self):
+        self.current_action_string = 'Selecting Max'
+        self.selection = 1
+
     def on_touch_down(self, touch):
-        # Normalized positions
+        super(CalibrationScreen, self).on_touch_down(touch)
+
+        # Normalized position on the image
         normalized_pos = [touch.x - self.image_offset[0], touch.y - self.image_offset[1]]
         normalized_pos[0] = normalized_pos[0] / self.image_dimensions[0]
         normalized_pos[1] = normalized_pos[1] / self.image_dimensions[1]
 
-        # Check if image was clicked
-        if(normalized_pos[0] > 1 or normalized_pos[0] < 0 or
+        # Check if image was clicked and selection in progress
+        if(self.selection == -1 or
+                   normalized_pos[0] > 1 or normalized_pos[0] < 0 or
                    normalized_pos[1] > 1 or normalized_pos[1] < 0):
+            self.selection = -1
             return
 
+        # TODO
+        # PIL has an issue with loading the file because it's truncated.
+        # Despite the "Image.LOAD_TRUNCATED_IMAGES = True", which should make it load
+        # the image regardless, it flat out refuses. I haven't been able to debug it
+        # and it is possible it is specific only to my system.
+        # I have not been able to test that the correct pixel is being grabbed
+        # since the pixel function doesn't work, but it looks right.
+        #
+        # To enable the intended functionality, uncomment lines 90 and 97, and
+        # remove the zeroed rgb assignments
+
         img = Image.open('calibImage.jpg')
+        #img.load()
 
         img_x = normalized_pos[0] * img.width
         img_y = normalized_pos[1] * img.height
         # Flip y so down is +
         img_y = img.height - img_y
+
+        #r, g, b = img.getpixel((img_x, img_y))
+        r, g, b = 0, 0, 0
+
+        if(self.selection == 0):
+            self.color.set_min([r, g, b])
+        if(self.selection == 1):
+            self.color.set_max([r, g, b])
+
+        self.selection = -1
+        self.current_action_string = ''
+        self.update_color_string()
+
+
 
 
 
