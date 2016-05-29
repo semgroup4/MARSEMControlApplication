866e58d0820149417234716399220a70b04cb0b0
Måns Thörnvik <thornvik.mans@gmail.com>
2016-05-29 11:56:30 +0200

    Added a Status widget that will show if its associated "thing" is on or off

--- marsem/gui/homeScreen.py
+++ marsem/gui/homeScreen.py
@@ -5,7 +5,8 @@
 from kivy.clock import Clock
 from kivy.graphics.texture import Texture
 from kivy.lang import Builder
-from kivy.properties import BooleanProperty
+from kivy.uix.widget import Widget
+from kivy.properties import BooleanProperty, NumericProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
 from kivy.uix.screenmanager import Screen
@@ -88,23 +89,50 @@ class OpenCVStream(BoxLayout):
         Clock.schedule_interval(self.update, 0.1)
 
 
+class Status(Widget):
+    # Assume that nothing is enabled and assign r, g, b and o to values you want as default
+    # color for the widget "thing", whatever it might be (does not necessarily have to
+    # inherit from 'Widget'.
+    enabled = BooleanProperty(False)
+    r = NumericProperty(0.988)
+    g = NumericProperty(0.043)
+    b = NumericProperty(0)
+    o = NumericProperty(0.5)
+
+    # 'value' is the 'enabled' BooleanProperty. Since we named the property 'enabled', the
+    # function that will listen to a changed value HAS TO BE NAMED 'on_enabled'!
+    def on_enabled(self, instance, value):
+        if value:
+            self.r = 0.227
+            self.g = 1
+            self.b = 0.082
+            self.o = 1
+        else:
+            self.r = 0.988
+            self.g = 0.043
+            self.b = 0
+            self.o = 0.5
+
 
 class PhotoProgress(ProgressBar):
     def __init__(self, **kwargs):
         super(PhotoProgress, self).__init__(**kwargs)
+        self.value = 0
         self.max = 600
 
     def start(self):
+        self.value = 0
         self.schedule_update()
 
     def schedule_update(self):
-        # Remove if already existing.
-        Clock.unschedule(self.update)
+        Clock.unschedule(self.update) # Remove if already existing.
         Clock.schedule_interval(self.update, 0.1)
+
+    def stop(self):
+        Clock.unschedule(self.update)
 
     def update(self, dt):
         self.value += 1
 
         if self.value >= 600:
-            self.value = 0
             return False
